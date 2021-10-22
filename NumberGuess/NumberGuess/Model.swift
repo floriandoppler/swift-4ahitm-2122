a7c228761d81f75d99f6f02989969442490a60ba
Florian <floriandoppler@icloud.com>
2021-10-22 09:28:57 +0200

    Initial commit

---
+++ NumberGuess/NumberGuess/Model.swift
@@ -0,0 +1,41 @@
+class Model {
+    var numberToGuess = 0
+    var countOfTries = 1
+    
+    func compare(guess guessedNumber:Int!) -> Int! {
+        var result = 0
+        
+        if guessedNumber < numberToGuess {
+            result = -1
+        }
+        
+        else if guessedNumber > numberToGuess {
+            result = 1
+        }
+        
+        return result        
+    }
+    
+    func compare(guessedString:String!) -> Int! {
+        let guess = Int(guessedString)
+        return compare(guess: guess)
+    }
+    
+    func isValid(guess guessedString:String?) -> Bool! {
+        var result = false
+        
+        if let str = guessedString {
+            let guess = Int(str)
+        
+            if guess != nil {
+                if let number = Int(str) {
+                    if number > 0 && number < 100 {
+                        result = true
+                    }
+                }
+            }
+        return result
+    }
+    }
+}
+
---
+++ NumberGuess/NumberGuess/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  NumberGuess
+//
+//  Created by Florian Doppler on 01.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var model = Model()
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var guessMeButton: UIButton!
+    @IBAction func buttonClick(_ sender: Any) {
+        // TODO: Check input form for not nil
+        
+        let guessedNumber = model.compare(guessedString: textField.text!)
+        
+        let text : String?
+        
+        switch model.compare(guess: guessedNumber) {
+        case -1:
+            text = "Higher"
+        case 1:
+            text = "Lower"
+        default:
+            text = "Correct"
+        }
+        
+        numberLabel.text = text
+        model.countOfTries += 1
+    }
+    
+    
+    @IBAction func editingChanged(_ sender: UITextField) {
+        guessMeButton.isEnabled = model.isValid(guess: sender.text)
+    }
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        model.numberToGuess = Int.random(in: 0..<100)
+        print("Number to guess: \(model.numberToGuess)")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
