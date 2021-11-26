073382cc652f20b23c44c60818754fcb05b05f53
Doppler Florian <it180182@WS8A03.htl-leonding.ac.at>
2021-11-26 09:38:46 +0100

    Started PersonenVerwaltung

---
+++ PersonenVerwaltung/PersonenVerwaltung/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonenVerwaltung
+//
+//  Created by Doppler Florian on 26.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var submitButton: UIButton!
+    submitButton.cornerRadius = 10
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
